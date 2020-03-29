39dea7b34f479836790aa2bb1308225ea317fc38
Nathan Cheung <nathancheung22@gmail.com>
2020-03-29 04:15:48 -0400

    added progress bar

--- public/js/player.js
+++ public/js/player.js
@@ -24,7 +24,8 @@ function onYouTubeIframeAPIReady(vId = "WFcjKjTq178") {
     playerVars: {
       controls: 0,
       modestbranding: 1,
-      autoplay: 0
+      autoplay: 1,
+      disablekb: 1
     }
   });
 }
@@ -50,13 +51,6 @@ function stopVideo() {
   player.stopVideo();
 }
 
-// checks for enter pressed on form input tag
-document.getElementById("video-link").onkeydown = function(event) {
-  if (event.keyCode == 13) {
-    loadNewVideo();
-  }
-};
-
 // parses link into videoId
 function parseVideoLink(link) {
   return link.split("=")[1].split("&")[0];
@@ -69,3 +63,70 @@ function loadNewVideo() {
 
   player.loadVideoById({ videoId: vId });
 }
+
+// waiting functions
+// checks for enter pressed on form input tag
+document.getElementById("video-link").onkeydown = function(event) {
+  if (event.keyCode == 13) {
+    loadNewVideo();
+  }
+};
+
+// on "submit" button click
+document.getElementById("submit-video-link").onclick = function() {
+  loadNewVideo();
+};
+
+// play && pause
+document.getElementById("play-pause-button").onclick = function() {
+  if (player.getPlayerState() == 1) {
+    player.pauseVideo();
+
+    // changes button icon
+    document
+      .getElementById("play-pause-button")
+      .getElementsByTagName("i")[0].className = "fa fa-play-circle";
+  } else {
+    player.playVideo();
+
+    // changes button icon
+    document
+      .getElementById("play-pause-button")
+      .getElementsByTagName("i")[0].className = "fa fa-pause-circle";
+  }
+};
+
+// skip back 5 seconds
+document.getElementById("skip-backward").onclick = function() {
+  const time = player.getCurrentTime();
+  player.seekTo(time - 5);
+};
+
+// skip forward 5 seconds
+document.getElementById("skip-forward").onclick = function() {
+  const time = player.getCurrentTime();
+  player.seekTo(time + 5);
+};
+
+//test functions
+// function updateScrubber() {
+//   console.log("working");
+//   if (player.getPlayerState() == 1 && videoLength > 0) {
+//     const percentage = (player.getCurrentTime() / videoLength) * 100;
+//     const s = "width: " + percentage.toString() + "%";
+//     console.log(s);
+//     document.getElementById("video-progress-bar").setAttribute("style", s);
+//   }
+//   console.log("test");
+// }
+
+// loop
+window.setInterval(function() {
+  const videoLength = player.getDuration();
+  if (player.getPlayerState() == 1 && videoLength > 0) {
+    const percentage = (player.getCurrentTime() / videoLength) * 100;
+    const s = "width: " + percentage.toString() + "%";
+    console.log(s);
+    document.getElementById("video-progress-bar").setAttribute("style", s);
+  }
+}, 200);
